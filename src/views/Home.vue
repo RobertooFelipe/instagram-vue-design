353651b044e5218fe5e25fbce2c817bb0b8c3c6c
Roberto Felipe <feliperoberto092@gmail.com>
2021-10-20 19:25:17 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,27 +2,343 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+       <div id="imagens"> 
+        <div id="instagram">
+          <img src="./instagram.png">
+        </div>
+        <div id="like">
+          <img src="./post.png">
+          <img src="./like.png">
+          <img src="./menssager.png">
+        </div>  
+       </div> 
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+
+      <div id="stories">
+        <img id="fotostorys" src="./me.png">
+        <img id="fotostorys" src="./hector.png">
+        <img id="fotostorys" src="./medina.png">
+        <img id="fotostorys" src="./norberto.png">
+        <img id="fotostorys" src="./victor.png">
+        <img id="fotostorys" src="./toso.png">
+        <img id="fotostorys" src="./luiz.png">
+        <img id="fotostorys" src="./felipe.png">
+        <img id="fotostorys" src="./akon.png">
+      </div>  
     
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./hector.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>_lopessszz</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./hector.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>523 curtidas</strong></p><br>
+       <p id="pa"><strong>_lopessszz</strong> dia após dia</p><br>
+       <p id="comentários">Ver todos os 15 comentários</p>
+       <p id="tempopost">há 1 hora •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./norberto.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>noberto_savioli</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./norberto.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>2.764 curtidas</strong></p><br>
+       <p id="pa"><strong>noberto_savioli</strong> curtindo com a família</p><br>
+       <p id="comentários">Ver todos os 241 comentários</p>
+       <p id="tempopost">há 17 horas •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./thiago.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>thi.giacobelli</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./thiago.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>467 curtidas</strong></p><br>
+       <p id="pa"><strong>thi.giacobelli</strong> reelembrando a infância</p><br>
+       <p id="comentários">Ver todos os 16 comentários</p>
+       <p id="tempopost">há 1 dia •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./me.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>pedrotosoorlando</p>  
+        </div>
+        <div>
+          <img src="./toso.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./toso.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>701 curtidas</strong></p><br>
+       <p id="pa"><strong>pedrotosoorlando</strong> jogando com os amigos</p><br>
+       <p id="comentários">Ver todos os 3 comentários</p>
+       <p id="tempopost">há 1 dia •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./victor.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>victor_negre</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./victor.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>69 curtidas</strong></p><br>
+       <p id="pa"><strong>victor_negre</strong> Em mato alto cobra ataca. Cuidado!!!</p><br>
+       <p id="comentários">Ver todos os 24 comentários</p>
+       <p id="tempopost">há 2 dias •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./akon.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>akon.oficial</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./akon.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>93.324 curtidas</strong></p><br>
+       <p id="pa"><strong>akon.oficial</strong> smack that</p><br>
+       <p id="comentários">Ver todos os 47.342 comentários</p>
+       <p id="tempopost">há 3 dias •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./luiz.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>feh_zoio</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./luiz.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>340 curtidas</strong></p><br>
+       <p id="pa"><strong>feh_zoio</strong> to no high</p><br>
+       <p id="comentários">Ver todos os 28 comentários</p>
+       <p id="tempopost">há 3 dias •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+      <section id="section">
+       <div id="conteudotop"> 
+        <div>
+          <img id="fotoperfil" src="./felipe.png">
+        </div>
+        <div id="nomeusuario">  
+          <p>femoreira_01</p>  
+        </div>
+        <div>
+          <img src="./options.png">
+        </div>
+       </div>
+
+       <img class="ftfeed" src="./felipe.png">
+
+       <div id="conteudofeedinf"> 
+        <div>
+          <img src="./like.png">
+          <img src="./coment.png">
+          <img src="./send.png">
+        </div>
+        <div>
+          <img src="./save.png">
+        </div>
+       </div>
+        <div id="descricao">
+       <p id="pa"><strong>15 curtidas</strong></p><br>
+       <p id="pa"><strong>femoreira_01</strong> tudo depende da flexibilidade do rabo da largatixa</p><br>
+       <p id="comentários">Ver todos os 28 comentários</p>
+       <p id="tempopost">há 3 dias •<strong> Ver tradução</strong></p>
+        </div>
+      </section>
+
+
+      <div id="bottom"></div>
+
+
+
+
+
+
+      <ion-toolbar id="barrabaixo">
+        <div id="iconsinf">
+          <div id="home">
+            <img src="./home.png">
+          </div>
+          <div id="search">
+            <img src="./search.png">
+          </div>
+          <div id="reels">
+            <img src="./reels.png">
+          </div>
+          <div id="store">
+            <img src="./store.png">
+          </div>
+          <div id="me">
+            <img id="ftperfild" src="./me.png">
+          </div>
+          
+        </div>
+      </ion-toolbar> 
+      
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import { IonContent, IonHeader, IonPage, IonToolbar } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -31,38 +347,102 @@ export default defineComponent({
     IonContent,
     IonHeader,
     IonPage,
-    IonTitle,
     IonToolbar
   }
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+*{
+font-family: 'Open Sans', sans-serif;
+font-size: 13px;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+#fotostorys{
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+#stories{
+  display: flex;
+  overflow-x: scroll;
 }
 
-#container a {
-  text-decoration: none;
+#tempopost *, #tempopost{
+  font-size: 10px;
+  opacity: 90%;
 }
+
+#bottom{
+  margin-bottom: 90px;
+}
+
+#pa, #comentários{
+  margin-top: -20px;
+}
+
+#descricao{
+  padding-left: 15px;
+  margin-top: 20px;
+}
+
+#comentários{
+  opacity: 50%;
+}
+
+#ftperfild{
+  border-radius: 50%;
+}
+
+#conteudotop{
+  margin-bottom: -25px;
+}
+
+#nomeusuario{
+  margin-right: 180px;
+  margin-top: 15px;
+}
+
+#fotoperfil{
+  border-radius: 50px;
+  height: 35px;
+}
+
+.ftfeed{
+  height: 400px;
+  margin-left: 0px;
+}
+
+#barrabaixo{
+  position:fixed;
+  left:0;
+  bottom:0;
+  width: 100%;
+  height: 8%;
+}
+
+img{
+  height: 25px;
+  margin: 10px;
+  margin-top: 20px;
+}
+
+#conteudofeedinf{
+  margin-top: -25px;
+}
+
+#imagens, #iconsinf, #conteudotop, #conteudofeedinf{
+  display: flex;
+  justify-content: space-between;
+}
+
+#iconsinf{
+  height: 35px;
+  margin: 10px;
+  margin-top: -25px;
+}
+
 </style>
